5e38d8141b91a92bd5da07d8916fe261042fcbb8
David Linca <alexlinca_21@yahoo.ro>
2024-12-04 17:19:56 +0200

    Lab8

--- App.xaml.cs
+++ App.xaml.cs
@@ -7,14 +7,19 @@ namespace Linca_David_Lab7
     public partial class App : Application
     {
         static ShoppingListDatabase database;
-        public static ShoppingListDatabase Database 
+        public static ShoppingListDatabase Database
         {
-            get 
-            { 
-                if (database == null) 
-                { 
-                    database = new 
-                    ShoppingListDatabase(Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "ShoppingList.db3")); } return database; } }
+            get
+            {
+                if (database == null)
+                {
+                    database = new
+                   ShoppingListDatabase(Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.
+                   LocalApplicationData), "ShoppingList.db3"));
+                }
+                return database;
+            }
+        }
         public App()
         {
             InitializeComponent();
